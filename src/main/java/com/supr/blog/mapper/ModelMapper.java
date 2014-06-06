049a03d0c779d58c521210be40dd0515278de4ec
coriger <461504978@qq.com>
2014-06-06 14:54:13 +0800

    新增模型数据单元使用的方法

--- src/main/java/com/supr/blog/mapper/ModelMapper.java
+++ src/main/java/com/supr/blog/mapper/ModelMapper.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 import com.supr.blog.model.cmge.Algorithm;
 import com.supr.blog.model.cmge.AlgorithmProvide;
+import com.supr.blog.model.cmge.DataType;
 import com.supr.blog.model.cmge.Model;
 import com.supr.blog.model.cmge.ModelAttr;
 import com.supr.blog.model.cmge.ModelDataUnit;
 import com.supr.blog.model.cmge.ModelLat;
 import com.supr.blog.model.cmge.Trade;
+
 
 public interface ModelMapper {
 
@@ -54,4 +56,19 @@ public interface ModelMapper {
 	int updateModelLat(ModelLat modelLat);
 
 	List<AlgorithmProvide> getAlgorithmProvideListById(Integer daId);
+
+	List<Model> getAllModelList();
+	
+	List<DataType> getDataTypeList();
+
+	List<ModelDataUnit> getModelDataUnitList(Map<String,Object> map);
+
+	int getModelDataUnitCount(Map<String,Object> map);
+
+	int deleteModelDataUnitBatch(String[] ids);
+
+	ModelDataUnit getModelDataUnitById(String modelId);
+
+	int saveModelDataUnitInfo(ModelDataUnit modelDataUnit);
+
 }
